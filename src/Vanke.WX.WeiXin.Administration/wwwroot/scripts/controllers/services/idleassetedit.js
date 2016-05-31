965321fd7a16c28d415235d5d947956d5d590242
Eamon Zhao <Eamon Zhao@DESKTOP-2CSD8J7>
2016-05-31 21:02:41 +0800

    Temp

---
+++ src/Vanke.WX.WeiXin.Administration/wwwroot/scripts/controllers/services/idleassetedit.js
@@ -0,0 +1,22 @@
+﻿(function (angular, app) {
+    'use strict';
+
+    app.controller('IdleAssetEditCtrl', function ($scope, $state, $stateParams, api) {
+        if (!angular.isUndefined($stateParams.id)) {
+            api.idleAssets.get({ id: $stateParams.id }, function (result) {
+                $scope.data = result;
+            });
+        }
+
+        $scope.save = function (form) {
+            form.$setSubmitted(true);
+
+            if (form.$valid) {
+                api.idleAssets.save($scope.data, function () {
+                    $state.go('service.idleassets');
+                });
+            }
+        };
+    });
+
+})(angular, app);
