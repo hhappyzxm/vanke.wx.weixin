89d45d3e7b2379828dfae9e95a27168a5baad85f
赵旭明 <hhappyzxm@outlook.com>
2016-06-03 21:19:29 +0800

    Temp

---
+++ src/Vanke.WX.Weixin/Controllers/FilesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using EZ.Framework.Integration.WebApi;
+
+namespace Vanke.WX.Weixin.Controllers
+{
+    public class FilesController : GenericApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> Upload()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                return BadRequest("Unsupported media type");
+            }
+            try
+            {
+                var provider = new MultipartFormDataStreamProvider(HttpRuntime.AppDomainAppPath + @"\Temp");
+
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                var photos =
+                  provider.FileData
+                    .Select(file => new FileInfo(file.LocalFileName))
+                    .Select(fileInfo => new 
+                    {
+                        Name = fileInfo.Name,
+                        Created = fileInfo.CreationTime,
+                        Modified = fileInfo.LastWriteTime,
+                        Size = fileInfo.Length / 1024
+                    }).ToList();
+                return Ok(new { Message = "Photos uploaded ok", Photos = photos });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.GetBaseException().Message);
+            }
+        }
+    }
+}
