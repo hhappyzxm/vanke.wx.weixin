2e080e7e8654062f6a5cc870310c4f15ca647e72
赵旭明 <hhappyzxm@outlook.com>
2016-06-04 18:57:15 +0800

    Temp

---
+++ src/Vanke.WX.WeiXin.Administration/wwwroot/scripts/controllers/files.js
@@ -0,0 +1,32 @@
+﻿(function (angular, app) {
+    'use strict';
+
+    app.controller('FilesCtrl', function ($rootScope, $scope, Upload, authService, sweetAlert) {
+        
+        function uploadUsing$http(file) {
+            file.upload = Upload.http({
+                url: 'http://localhost:54843/api/files',
+                method: 'POST',
+                headers:
+                  {
+                      'Content-Type': undefined//file.type
+                  },
+                data: file
+            });
+
+            file.upload.then(function (response) {
+                file.result = response.data;
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            });
+
+            //file.upload.progress(function (evt) {
+            //    file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            //});
+        }
+
+        $scope.upload = uploadUsing$http;
+    });
+
+})(angular, app);
