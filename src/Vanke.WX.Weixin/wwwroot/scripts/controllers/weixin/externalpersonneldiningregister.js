94d78aa897d322964d2c5b623a934a0b2d93128f
Eamon Zhao <Eamon Zhao@DESKTOP-2CSD8J7>
2016-06-11 22:43:42 +0800

    Temp

--- src/Vanke.WX.Weixin/wwwroot/scripts/controllers/weixin/externalpersonneldiningregister.js
+++ src/Vanke.WX.Weixin/wwwroot/scripts/controllers/weixin/externalpersonneldiningregister.js
@@ -2,7 +2,15 @@
     'use strict';
 
     app.controller('ExternalPersonnelDiningRegisterCtrl', function ($scope, api) {
+        $scope.save = function (form) {
+            form.$setSubmitted(true);
 
+            if (form.$valid) {
+                api.externalPersonnelDiningRegister.save($scope.data, function () {
+                    //$state.go('settings.dinnerplaces');
+                });
+            }
+        };
     });
 
 })(angular, window.app);
