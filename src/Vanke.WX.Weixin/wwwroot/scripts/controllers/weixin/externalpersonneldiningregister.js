63c2db0224b8d212b043c0ca9c60f62ad06232ee
赵旭明 <hhappyzxm@outlook.com>
2016-06-12 08:43:26 +0800

    Temp

--- src/Vanke.WX.Weixin/wwwroot/scripts/controllers/weixin/externalpersonneldiningregister.js
+++ src/Vanke.WX.Weixin/wwwroot/scripts/controllers/weixin/externalpersonneldiningregister.js
@@ -1,13 +1,15 @@
 ﻿(function (angular, app) {
     'use strict';
 
-    app.controller('ExternalPersonnelDiningRegisterCtrl', function ($scope, api) {
+    app.controller('ExternalPersonnelDiningRegisterCtrl', function ($scope, api, sweetAlert) {
         $scope.save = function (form) {
             form.$setSubmitted(true);
 
             if (form.$valid) {
                 api.externalPersonnelDiningRegister.save($scope.data, function () {
-                    //$state.go('settings.dinnerplaces');
+                    sweetAlert.success('提交成功');
+                    form.$submitted = false;
+                    $scope.data = null;
                 });
             }
         };
